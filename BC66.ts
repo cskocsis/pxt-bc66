0294e3fb22b9f5a0c17e36fcc255138aaf6212f0
cskocsis <36304794+cskocsis@users.noreply.github.com>
2019-05-12 17:56:28 +0200

    Updates.

--- BC66.ts
+++ BC66.ts
@@ -217,7 +217,7 @@ namespace BC66 {
             return
         }
         ensureSocket()
-        writeCommand(`AT+QSOSEND=${socket},${buffer.length},"${buffer.toHex()}"`)
+        writeCommand(`AT+QSOSEND=0,${buffer.length},"${buffer.toHex()}"`)
     }
 
     /**
@@ -302,7 +302,7 @@ namespace BC66 {
         writeCommand(`AT+QSOC=1,2,1`)
         let response = readLine()
         socket = parseInt(response.charAt(6))
-        writeCommand(`AT+QSOCON=${socket},41235,"188.166.45.215"`)
+        writeCommand(`AT+QSOCON=0,41235,"188.166.45.215"`)
     }
 
     /**
