fbf4f239bb803cf7c6e7c70e283a1eb5b739cfc6
cskocsis <36304794+cskocsis@users.noreply.github.com>
2019-05-12 18:21:48 +0200

    redy

--- BC66.ts
+++ BC66.ts
@@ -303,7 +303,7 @@ namespace BC66 {
         writeCommand(`AT+QSOC=1,2,1`)
         let response = readLine()
         socket = parseInt(response.charAt(6))
-        writeCommand(`AT+QSOCON=0,41235,"188.166.45.215"`)
+        writeCommand(`AT+QSOCON=0,${serverPort},${serverIp}`)
     }
 
     /**
